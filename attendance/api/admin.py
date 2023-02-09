29d4e842ce83c299e05eaa636931ff03ccd9bb97
ThunderSmoker <pitlawararadhya@gmail.com>
2023-02-09 23:53:42 +0530

    added batch

--- attendance/api/admin.py
+++ attendance/api/admin.py
@@ -3,7 +3,6 @@ from .models import Student
 # Register your models here.
 class StudAdmin(admin.ModelAdmin):
     list_display=('date','prn','batch','present')
-    list_editable=('date','prn','batch')
     search_fields=('prn','batch')
-    list_filter=('batch')
+    list_filter=('batch',)
 admin.site.register(Student,StudAdmin)
