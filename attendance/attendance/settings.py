0ac013d58308b4a051e04f751e7a76d917426509
ThunderSmoker <pitlawararadhya@gmail.com>
2023-02-12 16:50:57 +0530

    req

--- attendance/attendance/settings.py
+++ attendance/attendance/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['wce-attendance.up.railway.app','127.0.0.1','*']
 
-
+CSRF_TRUSTED_ORIGINS = ['https://*.up.railway.app','https://*.127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
